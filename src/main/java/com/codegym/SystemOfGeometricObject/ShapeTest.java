8703b22c867cb0a751d2cc1300312d1dc8c08ee1
TienCGC8JAVA <nguyenmanhtien08081994@gmail.com>
2019-06-10 17:52:09 +0700

    Hệ các đối tượng hình học

---
+++ src/main/java/com/codegym/SystemOfGeometricObject/ShapeTest.java
@@ -0,0 +1,11 @@
+package com.codegym.SystemOfGeometricObject;
+
+public class ShapeTest {
+    public static void main(String[] args) {
+        Shape shape = new Shape();
+        System.out.println(shape);
+
+        shape = new Shape("red", false);
+        System.out.println(shape);
+    }
+}
